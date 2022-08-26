e8e81fffa994c6b50783de7bc0c3274226518367
Youling Tang <tangyouling@loongson.cn>
2022-08-26 14:29:53 +0800

    LoongArch: PE format image loading support
    The LoongArch kernel will mainly use the vmlinux.efi image in PE format,
    so add it support.
    
    Signed-off-by: Youling Tang <tangyouling@loongson.cn>

---
+++ kexec/arch/loongarch/image-header.h
@@ -0,0 +1,81 @@
+/*
+ * LoongArch binary image header.
+ */
+
+#if !defined(__LOONGARCH_IMAGE_HEADER_H)
+#define __LOONGARCH_IMAGE_HEADER_H
+
+#include <endian.h>
+#include <stdint.h>
+
+/**
+ * struct loongarch_image_header
+ *
+ * @pe_sig: Optional PE format 'MZ' signature.
+ * @branch_code: Reserved.
+ * @text_offset: The image load offset in LSB byte order.
+ * @image_size: An estimated size of the memory image size in LSB byte order.
+ * @flags: Reserved.
+ * @kernel_entry: Kernel image entry pointer.
+ * @reserved: Reserved.
+ * @magic: Reserved.
+ * @pe_header: Optional offset to a PE format header.
+ **/
+
+struct loongarch_image_header {
+	uint8_t pe_sig[2];
+	uint16_t branch_code[3];
+	uint64_t text_offset;
+	uint64_t image_size;
+	uint64_t flags;
+	uint64_t kernel_entry;
+	uint64_t reserved[2];
+	uint8_t magic[4];
+	uint32_t pe_header;
+};
+
+static const uint8_t loongarch_image_pe_sig[2] = {'M', 'Z'};
+
+/**
+ * loongarch_header_check_pe_sig - Helper to check the loongarch image header.
+ *
+ * Returns non-zero if 'MZ' signature is found.
+ */
+
+static inline int loongarch_header_check_pe_sig(const struct loongarch_image_header *h)
+{
+	if (!h)
+		return 0;
+
+	return (h->pe_sig[0] == loongarch_image_pe_sig[0]
+		&& h->pe_sig[1] == loongarch_image_pe_sig[1]);
+}
+
+static inline uint64_t loongarch_header_text_offset(
+	const struct loongarch_image_header *h)
+{
+	if (!h)
+		return 0;
+
+	return le64toh(h->text_offset);
+}
+
+static inline uint64_t loongarch_header_image_size(
+	const struct loongarch_image_header *h)
+{
+	if (!h)
+		return 0;
+
+	return le64toh(h->image_size);
+}
+
+static inline uint64_t loongarch_header_kernel_entry(
+	const struct loongarch_image_header *h)
+{
+	if (!h)
+		return 0;
+
+	return le64toh(h->kernel_entry);
+}
+
+#endif
